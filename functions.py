a06980d42873a582c26e77acab54186f7a74e9c6
killgriff22 <kilgrif22033@gmail.com>
2024-07-21 13:21:30 -0400

    misplaced split subfunction

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def change_audio_driver():
 @app.route('/')
 @app.route('/library')
 def library():
-    return "<br>".join(str(os.listdir(source_paths[0]).split(",")))
+    return "<br>".join(str(os.listdir(source_paths[0])).split(","))
 
 def Audio_Daemon():
     queue = []
