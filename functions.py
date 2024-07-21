cc5de33e06a1e624b9d55b268d1c591e610c09f5
killgriff22 <kilgrif22033@gmail.com>
2024-07-20 23:10:26 -0400

    add redirect

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from classes import *
 def change_audio_driver():
     os.putenv('SDL_AUDIODRIVER', 'alsa')
     os.putenv('SDL_AUDIODEV', '/dev/audio')
-
+@app.route('/')
 @app.route('/library')
 def library():
     return flask.render_template('playback.html')
