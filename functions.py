508322713296328100f6f73dd1376616f5d61482
killgriff22 <kilgrif22033@gmail.com>
2024-07-21 17:17:05 -0400

    migrate library to index.html

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def library():
     content = ""
     for song in os.listdir(source_paths[0]):
         content += f"<a href='/add/{song}'>{song}</a><br>"
-    return content
+    return flask.render_template("index",content=content)
 
 @app.route('/add/<string:song>')
 def add(song):
