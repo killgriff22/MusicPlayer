784e05106c6503e526053ae3f74011286dd09230
killgriff22 <kilgrif22033@gmail.com>
2024-07-21 13:22:50 -0400

    i forgot bits :/

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def change_audio_driver():
 @app.route('/')
 @app.route('/library')
 def library():
-    return ".mp3'<br>".join(str(os.listdir(source_paths[0])).split(".mp3'"))
+    return ".mp3'<br>".join(str(os.listdir(source_paths[0])).split(".mp3', "))
 
 def Audio_Daemon():
     queue = []
