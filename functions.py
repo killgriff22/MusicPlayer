9e0072a068c8e65978977ca1a073fcab0f37bdae
killgriff22 <kilgrif22033@gmail.com>
2024-07-21 13:11:45 -0400

    update webapp

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def change_audio_driver():
 @app.route('/')
 @app.route('/library')
 def library():
-    return flask.render_template('index.html', songs=[source_paths[0]+os.listdir(source_paths[0])[0]])
+    return flask.render_template('index.html', songs=str([source_paths[0]+os.listdir(source_paths[0])[0]]))
 
 def Audio_Daemon():
     queue = []
