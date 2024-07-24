0002121d4c21410da87c0dd724e3c5af1f83056b
killgriff22 <kilgrif22033@gmail.com>
2024-07-24 16:21:40 -0400

    commit to try on pi

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import os
 import time
 from functions import *
+from MultiTerm import *
+
 width,height = os.get_terminal_size()
 height-=1
 space=width*height
+topbar = Screen((width,1),(0,0))
+area = Screen((width,height-1),(0,1))
 if sys.argv[1].isdigit():
   string = sys.argv[1]
 else:
@@ -15,9 +19,26 @@ clear()
 steps=int(sys.argv[2])
 times = []
 x,y=0,0
+lastchr = ""
+count=0
+area.blit(string,(0,0))
+area.draw()
+input("Press Enter to start")
 while True:
+  if len(string) > space:
+    string = string[:space]
   if len(sys.argv) == 5 and steps-int(sys.argv[2]) == int(sys.argv[3]):
     with open(sys.argv[4],"w") as f:
       f.write(string)
       f.close()
     exit()
+  if string[x*y] == lastchr:
+      count+=1
+  else:
+      x-=count
+      string = string[:x*y]+str(count)+lastchr+string[x*y:]
+      x+=2
+      count=1
+      lastchr = string[x*y+1]
+  area.blit(string,(0,0))
+  area.draw()
